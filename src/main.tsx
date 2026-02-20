f069d98afb0c3e86f93dcdf9e2239302b2892707
JuanCRM <jcros84@gmail.com>
2026-02-20 11:35:51 +0100

    Instalacion de https://ui.shadcn.com/ e inicio de useReduce

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { createRoot } from 'react-dom/client'
 //import { HooksApp } from './HooksApp'
 
 import './index.css'
+import { TasksApp } from './05-useReducer/TasksApp'
 //import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 //import { NavBar } from './navbar'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,6 +21,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <NavBar /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+
+    <TasksApp />
   </StrictMode>
 )
